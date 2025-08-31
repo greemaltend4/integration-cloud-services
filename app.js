8ce768eabca13de9e42cb74c37f3b3eff1b07b4d
greemaltend4 <dengestabiy@hotmail.com>
2025-08-31 10:10:25 -0500

    Added a default port setting for the server

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const app = express();
 app.use(express.json());
 app.use('/api', apiRoutes);
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
