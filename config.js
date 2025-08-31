2120c05a3aa8a4b3669a6620cac044f1c97af39e
greemaltend4 <dengestabiy@hotmail.com>
2025-08-31 10:07:43 -0500

    feat: initial commit for config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+  },
+  gcp: {
+    projectId: process.env.GCP_PROJECT_ID,
+    keyFilename: process.env.GCP_KEY_FILE
+  },
+  azure: {
+    clientId: process.env.AZURE_CLIENT_ID,
+    clientSecret: process.env.AZURE_CLIENT_SECRET,
+    tenantId: process.env.AZURE_TENANT_ID
+  }
+};
